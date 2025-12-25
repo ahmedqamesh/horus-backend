fb37b12688d78b915bd03e98e1f9dc22f2e4e06b
Ahmed Qamesh <ahmed.qamesh@cern.ch>
2025-12-25 23:34:48 +0100

    updating per customer

--- routes/api.php
+++ routes/api.php
@@ -5,3 +5,8 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::group(['prefix' => 'v1'], function () {
+    Route::apiResource('customers', \App\Http\Controllers\Api\V1\CustomerController::class);
+    Route::apiResource('invoices', \App\Http\Controllers\Api\V1\InvoiceController::class);
+});
